587e2fb10e81a660d9320d359ecc7417b7355ac0
ulif <uli@gnufix.de>
2018-12-10 20:55:29 +0100

    Add a setup.py skeleton.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sb_frontend',
+      version='0.1dev0',
+      description='StalkerBuster web frontend',
+      author='ulif and datenzwerg.in',
+      author_email='stalkerbuster@gnufix.de',
+      url='https://github.com/stalkerbuster/sb_frontend',
+      package_dir = {'': 'src'},
+      packages=['sb_frontend'],
+     )
