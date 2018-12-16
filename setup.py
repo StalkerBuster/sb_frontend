da7cbc2ed16c3dc3b57df6ea54dc9919f01537d3
ulif <uli@gnufix.de>
2018-12-16 14:54:29 +0100

    Define test/install dependencies.
    We use py.test for testing. Tell what packages we need in setup.py, so
    that we can run `python setup.py test` to install and run tests.
    
    We deploy the manual py.test resgistration in setup.py, because the
    alternative "integrated" approach requires to set the setuptools
    `setup_requires` var, which consider to be bad. Instead we try to avoid
    using `setup_requires`.

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    # see
+    # https://docs.pytest.org/en/3.0.0/goodpractices.html#manual-integration
+    # to learn what happens here.
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        # late-import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(name='sb_frontend',
       version='0.1.dev0',
@@ -9,4 +29,7 @@ setup(name='sb_frontend',
       author_email='stalkerbuster@gnufix.de',
       url='https://github.com/stalkerbuster/sb_frontend',
       package_dir={'': 'src'},
+      install_requires=['flask'],
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
      )
