56dd99d02f78f68956a95f18ff32c13513a1e0d1
ulif <uli@gnufix.de>
2018-12-18 13:51:17 +0100

    Prove, that we return _something_ for root
    and not nothing.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,11 @@
 # tests for app.py
 import os
-from sb_frontend.app import THEME_PATH
+from sb_frontend.app import THEME_PATH, app
 
 
 def test_theme_path_exists():
     assert os.path.exists(THEME_PATH)
+
+def test_index():
+    resp = app.test_client().get("/")
+    assert resp.status == '200 OK'
