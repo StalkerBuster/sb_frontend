cfd3034a84301daec03ab4cc053683542e9e7ea9
ulif <uli@gnufix.de>
2018-12-19 01:35:46 +0100

    pep8, style.

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,8 @@ from sb_frontend.app import THEME_PATH, app
 def test_theme_path_exists():
     assert os.path.exists(THEME_PATH)
 
-def test_index():
+
+def test_app_root_reachable():
+    # ensure, the root of our web app can be reached
     resp = app.test_client().get("/")
     assert resp.status == '200 OK'
