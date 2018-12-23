7145626667d0d17fd34df08cbaf5ae43572bdc39
ulif <uli@gnufix.de>
2018-12-23 02:49:19 +0100

    Add test to ensure we can reach the static content.

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,12 @@ def test_theme_path_exists():
     assert os.path.exists(THEME_PATH)
 
 
+def test_static_content_available():
+    # we can get content on the `/static` path
+    resp = app.test_client().get("/static/index.html")
+    assert resp.status == "200 OK"
+
+
 def test_app_root_reachable():
     # ensure, the root of our web app can be reached
     resp = app.test_client().get("/")
