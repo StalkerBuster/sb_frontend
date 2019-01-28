69d89d031dfabec1d221ccb2268b7c2d541b41c0
datenzwergin <datenzwergin@localhost.localdomain>
2019-01-28 18:46:50 +0100

    Login

--- src/sb_frontend/app.py
+++ src/sb_frontend/app.py
@@ -17,9 +17,11 @@
 """
 import os
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort, make_response
 
 app = Flask(__name__)
 app.root_ca_path = None
+app.secret_key = 'StalkerBuster'
 
 
 THEME_PATH = os.path.abspath(
@@ -33,13 +35,47 @@ TEMPLATES_PATH = os.path.abspath(
 @app.route('/')
 def index():
     content = open(os.path.join(TEMPLATES_PATH, "index.tmpl"), "r").read()
-    return content
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return content
 
 @app.route('/about')
 def about():
     content = open(os.path.join(TEMPLATES_PATH, "about.tmpl"), "r").read()
     return content
 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True 
+    else:
+        flash('wrong password')
+    return index() 
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return index()
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['username']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('SID', user)
+        return resp
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('SID')
+    return '<h1>welcome '+username+'</h1>'
+
+
 @app.route('/sb-root.crt')
 def sb_root_crt():
     return ""
+
+if __name__ =="__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host='0.0.0.0.', port=5000)
